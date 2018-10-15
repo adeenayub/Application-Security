432fb23a74980ab884aba8f7d3811d642e81617a
adeenayub <12beseaayub@seecs.edu.pk>
2018-10-16 00:07:35 +0500

    Update spellcheck.py
    Reads a .txt file and tells if any of the words are spelled incorrectly

--- Assignment 1/spellcheck.py
+++ Assignment 1/spellcheck.py
@@ -1,4 +1,11 @@
 import enchant
-d = enchant.Dict("en_US")
-d.check("Potatos")
-d.check("Potatoes")
+from enchant.checker import SpellChecker
+with open('data.txt', 'r') as myfile:
+	textread=myfile.read().replace('\n', '')
+txtchkr = SpellChecker("en_US")
+txtchkr.set_text(textread)
+for err in txtchkr:
+	print ("ERROR:" ,err.word)
+
+
+#print("Alhamdulillah")
